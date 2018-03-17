48addda601c4d3af83212a7b67fe5a39c2ca9b4a
James Greenleaf <james@greenle.af>
2018-03-17 19:50:42 -0400

    dont show the start time if the event started on a previous day

--- src/components/Day.jsx
+++ src/components/Day.jsx
@@ -3,18 +3,30 @@ import Event from 'app/components/Event';
 
 class Day extends Component {
   render() {
+    var { date, isTrailing } = this.props;
     let events = [];
     if (this.props.events) { // sort by start date
       events = this.props.events.slice(); // so we don't modify props
       events.sort((x, y) => x.start < y.start ? -1 : 1);
     }
 
+    var dayStart = new Date(date);
+    dayStart.setHours(0);
+    dayStart.setMinutes(0);
+    dayStart.setSeconds(0);
+    var dayEnd = new Date(date);
+    dayEnd.setHours(23);
+    dayEnd.setMinutes(59);
+    dayEnd.setSeconds(59);
+
     return (
-      <div className={`day ${ this.props.isTrailing ? 'trailing' : 'normal'}`} data-date={this.props.date.toDateString()}>
-        <div className="day-number">{this.props.date.getDate()}</div>
-        {events.map((event, i) =>
-          <Event key={i} name={event.name} start={event.start} end={event.end} />
-        )}
+      <div className={`day ${ isTrailing ? 'trailing' : 'normal'}`} data-date={date.toDateString()}>
+        <div className="day-number">{date.getDate()}</div>
+        {events.map((event, i) => {
+          return (
+            <Event key={i} name={event.name} start={event.start} end={event.end} showStart={event.start > dayStart} />
+          );
+        })}
       </div>
     );
   }
--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -2,15 +2,20 @@ import React, {Component} from 'react';
 
 class Event extends Component {
   render() {
-    const { start } = this.props;
-    let hour = `${start.getHours()}`.padStart(2, '0');
-    let minute = `${start.getMinutes()}`.padStart(2, '0');
-    let startTime = `${hour}:${minute}`;
+    const { start, name, showStart } = this.props;
+
+    let startDisplay;
+    if (showStart) {
+      let hour = `${start.getHours()}`.padStart(2, '0');
+      let minute = `${start.getMinutes()}`.padStart(2, '0');
+      let startTime = `${hour}:${minute}`;
+      startDisplay = <span className="event-start-time">{startTime}</span>;
+    }
 
     return (
       <div className="event">
-        <span className="event-start-time">{startTime}</span>
-        <span className="event-name">{this.props.name}</span>
+        {startDisplay}
+        <span className="event-name">{name}</span>
       </div>
     );
   }

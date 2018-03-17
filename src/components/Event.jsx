22867b025a8c21c7e65b5a10230179e6d8bb3ff2
James Greenleaf <james@greenle.af>
2018-03-17 17:37:37 -0400

    event start times

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -2,8 +2,16 @@ import React, {Component} from 'react';
 
 class Event extends Component {
   render() {
+    const { start } = this.props;
+    let hour = `${start.getHours()}`.padStart(2, '0');
+    let minute = `${start.getMinutes()}`.padStart(2, '0');
+    let startTime = `${hour}:${minute}`;
+
     return (
-      <div className="event">{this.props.name}</div>
+      <div className="event">
+        <span className="event-start-time">{startTime}</span>
+        <span className="event-name">{this.props.name}</span>
+      </div>
     );
   }
 }
